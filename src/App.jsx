c606a87365b74be27f9b498c2b846abf6abc3d14
ali <alimotahari565@gmail.com>
2023-10-12 02:37:23 +0330

    add timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import image1Url from "../src/assets/image-1.png";
 import image2Url from "../src/assets/image-2.png";
 import image3Url from "../src/assets/image-3.png";
@@ -7,7 +7,6 @@ import image5Url from "../src/assets/image-5.png";
 import image6Url from "../src/assets/image-6.png";
 import image7Url from "../src/assets/image-7.png";
 import image8Url from "../src/assets/image-8.png";
-
 
 const images = [
   image1Url,
@@ -27,19 +26,43 @@ function App() {
   const [selectedItemIds, setSelectedItemIds] = useState([]);
   const [numberClick, setNumberClick] = useState(0);
   const [numberClick2, setNumberClick2] = useState(40);
+  const [time, setTime] = useState(120);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let timer = null;
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTime((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+    }
+    return() => {
+      clearInterval(timer)
+    }
+  }, [isRunning]);
+
+  const formatTime = () => {
+    if (time > 0) {
+        const minutes = Math.floor(time / 60);
+        const remainingSeconds = time % 60;
+        return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    }
+}
   const generateRandom = () =>
-      Array.from({ length: 16 }).map((_, i) => {
+    Array.from({ length: 16 })
+      .map((_, i) => {
         return {
           id: i + 1,
           identifier: Math.floor(i / 2),
           image: images[Math.floor(i / 2)],
         };
       })
-          .sort(() => Math.random() - 0.5)
+      .sort(() => Math.random() - 0.5);
   const [items, setItems] = useState(generateRandom);
 
   const handleClick = (item) => {
-    if (numberClick < 2 && numberClick2 > 0) {
+    if (numberClick < 2 && numberClick2 > 0 && time > 0) {
       setNumberClick2(numberClick2 - 1);
       setNumberClick(numberClick + 1);
       setSelectedItemIds([...selectedItemIds, item.id]);
@@ -57,49 +80,44 @@ function App() {
         } else {
           setNumberClick(0);
         }
+
       }
     }
   };
 
-  const resetGame= () => {
-    const newItems = generateRandom()
-    setSelectedItemIds(newItems.map((i)=>i.id));
-    setItems(newItems)
+  const resetGame = () => {
+    setIsRunning(true);
+    setTime(120)
+    const newItems = generateRandom();
+    setSelectedItemIds(newItems.map((i) => i.id));
+    setItems(newItems);
     setTimeout(() => {
-      setSelectedItemIds([])
-      setNumberClick2(40)
-    }, 1000);}
-
-    // if(numberClick2 === 0) {
-    //   resetGame()
-    // }
-
+      setSelectedItemIds([]);
+      setNumberClick2(40);
+    }, 1000);
+  };
 
   return (
-      <div className='game-box'>
-        <div className='time-move'>
-          <div>
-            زمان : 2:00
-          </div>
-          <div>
-            تعداد حرکت :  {numberClick2}
-          </div>
-        </div>
-        <div className="memory-game">
-          {items.map((item, i) => (
-              <Item
-                  key={item.id}
-                  index={i + 1}
-                  image={item.image}
-                  onClick={() => handleClick(item)}
-                  isShow={selectedItemIds.includes(item.id)}
-              />
-          ))}
-        </div>
-        <button className="btn-reset" onClick={resetGame}>
-          شروع دوباره
-        </button>
+    <div className="game-box">
+      <div className="time-move">
+        <div>زمان : {time > 0 ? formatTime() : '0:00'}</div>
+        <div>تعداد حرکت : {numberClick2}</div>
       </div>
+      <div className="memory-game">
+        {items.map((item, i) => (
+          <Item
+            key={item.id}
+            index={i + 1}
+            image={item.image}
+            onClick={() => handleClick(item)}
+            isShow={selectedItemIds.includes(item.id)}
+          />
+        ))}
+      </div>
+      <button className="btn-reset" onClick={resetGame}>
+        شروع دوباره
+      </button>
+    </div>
   );
 }
 
