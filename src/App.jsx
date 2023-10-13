fbe535296bafdb897c1b13bef2415aaacc7f968b
ali <alimotahari565@gmail.com>
2023-10-13 20:00:32 +0330

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -165,7 +165,7 @@ function App() {
       style={{
         background: theme === "dark" ? "#1d3557" : "#e9e9e9",
         transition: ".2s all",
-        width: "1000%",
+        width: "100%",
         height: "100vh",
       }}
     >
