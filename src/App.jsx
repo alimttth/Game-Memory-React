5a75b33d5ebd6cf525fb68526939dc9f206aa93a
Mohammad javad Jafar Abady <mjafary822@gmail.com>
2023-10-13 15:28:42 +0330

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
 
   const handleClick = (item) => {
     if (numberClick < 2 && numberClick2 > 0 && time > 0 && item.id!==selectedItemIds) {
-      if (!selectedItemIds.includes(item.id)) {
+      if (!selectedItemIds.includes(item.id)) {//این شرط اضافه شد.....
         setNumberClick2(numberClick2 - 1);
         setSelectedItemIds([...selectedItemIds, item.id]);
       }
